c96e9d98629c325ed7df0aa8b84c93b47f9d84b3
Ignacio Moreira Toja <ignaciomoreiratoja@outlook.com.ar>
2024-07-10 22:52:50 -0300

    corregido Spanglish y últimos cosméticos

--- src/NotFoundPage/NotFoundPage.jsx
+++ src/NotFoundPage/NotFoundPage.jsx
@@ -1,11 +1,21 @@
 import './NotFoundPage.css';
 
+import { useNavigate } from 'react-router-dom'
+
+
 const NotFoundPage = () => {
+
+    const navigate = useNavigate()
+
+    const handleGoHome = () => {
+        navigate('/');
+    };
+
     return (
         <div className='NotFoundContainer'>
-            <h1 className='NotFoundTitle'>404 - Página No Encontrada</h1>
-            <p className='NotFoundMessage'>Lo sentimos, la página que estás buscando no existe.</p>
-            <button className='NotFoundButton' onClick={() => window.history.back()}>Volver</button>
+            <h1 className="NotFoundTitle">404 - Page Not Found</h1>
+            <p className="NotFoundMessage">The page you are looking for does not exist.</p>
+            <button className='NotFoundButton' onClick={handleGoHome}>Go Home</button>
         </div>
     );
 };
